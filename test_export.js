fa5db162709dbbb5f824063d1bb2120e981eaaba
le.qu <le.qu@perkinelmer.com>
2023-12-29 15:44:00 +0800

    111

---
+++ test_export.js
@@ -0,0 +1,15 @@
+
+  window.addEventListener('load',function(){
+    var div = document.createElement('div');
+    // Step 2: Set the styling for the div
+    div.style.backgroundColor = '#11000011';
+    div.style.width = '10px';
+    div.style.height = '10px';
+    div.style.position = 'absolute';
+    div.style.top = '0%';
+    div.style.left = '40%';
+    div.style.transform = 'translate(-50%, 0)';
+
+    document.body.appendChild(div);
+
+  })
